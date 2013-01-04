a69aa7117b1af3463f8443149303fd4f84931a11
Will West <will@wpengine.com>
2013-01-04 11:16:30 -0600

    add wtf-post-status - error_log transitions and their backtraces

---
+++ wtf-post-status.php
@@ -0,0 +1,9 @@
+<?php
+
+function wtf_debug_tps($ns, $os, $post) {
+	error_log("WTF-DEBUG - TRANSITION - $post->ID from $os to $ns");
+	error_log("WTF-DEBUG - TRACE - " . print_r(debug_backtrace(FALSE),1));
+}
+
+add_action('transition_post_status', 'wtf_debug_tps');
+?>
