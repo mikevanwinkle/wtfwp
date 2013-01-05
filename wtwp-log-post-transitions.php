a38e829cd32a3a01531a57a27f321ae66dc70427
Will West <will@wpengine.com>
2013-01-04 19:59:25 -0600

    fix single log message and shortened traces

--- wtwp-log-post-transitions.php
+++ wtwp-log-post-transitions.php
@@ -1,8 +1,12 @@
 <?php
 
 function wtf_debug_tps($ns, $os, $post) {
-	error_log("WTF-DEBUG - TRANSITION - $post->ID from $os to $ns");
-	error_log("WTF-DEBUG - TRACE - " . print_r(debug_backtrace(FALSE),1));
+	$bigtrace = debug_backtrace();
+	$trace = array(); // manually construct trace w/o args for older php 5.3 compat
+	foreach($bigtrace as $frame) {
+		$trace[] = array($frame['file'], $frame['line'], $frame['function']);
+	}
+	error_log("WTWP - POST TRANSITION - $post->ID from [$os] to [$ns] at " . print_r($trace,1));
 }
 
 add_action('transition_post_status', 'wtf_debug_tps', 0, 3);
