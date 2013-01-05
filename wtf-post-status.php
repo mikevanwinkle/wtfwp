b760de8d773df650a5b0fc84de0f3dc9693a726a
Will West <will@wpengine.com>
2013-01-04 19:36:44 -0600

    fix - accept three args in add_action

--- wtf-post-status.php
+++ wtf-post-status.php
@@ -5,5 +5,5 @@ function wtf_debug_tps($ns, $os, $post) {
 	error_log("WTF-DEBUG - TRACE - " . print_r(debug_backtrace(FALSE),1));
 }
 
-add_action('transition_post_status', 'wtf_debug_tps');
+add_action('transition_post_status', 'wtf_debug_tps', 0, 3);
 ?>
