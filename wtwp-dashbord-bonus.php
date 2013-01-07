8e192194e8f21e92571b00b407a2d162dea813c1
Will West <will@wpengine.com>
2013-01-07 10:17:00 -0600

    add system status checks for options and transients

--- wtwp-dashbord-bonus.php
+++ wtwp-dashbord-bonus.php
@@ -1,14 +1,11 @@
 <?php
 
-function wtwp_echo_constants_table() {
-	$all = get_defined_constants(TRUE); // categorized
-	$all = $all['user']; // just what we can control
+function wtwp_echo_array_table($vals) {
 	echo("<table>\n");
-	foreach($all as $name => $value) {
+	foreach($vals as $name => $value) {
 		$safe_name = esc_html($name);
 		$safe_value = is_array($value) ? "!!array-" . count($value) : 
 						is_object($value) ? "!!object-" .get_class($value) :
-						is_resource($value) ? "!!resource-" . get_resource_type($value) :
 						is_null($value) ? "!!null" :
 						is_bool($value) ? "!!bool-" . ($value ? 'TRUE' : 'FALSE') :
 						 esc_html($value);
@@ -17,23 +14,37 @@ function wtwp_echo_constants_table() {
 	echo("</table>\n");
 }
 
+function wtwp_echo_constants_table() {
+	$all = get_defined_constants(TRUE); // categorized
+	$all = $all['user']; // just what we can control
+	wtwp_echo_array_table($all);
+}
+
 function wtwp_echo_globals_table() {
-	echo("<table>\n");
-	foreach($GLOBALS as $name => $value) {
-		$safe_name = esc_html($name);
-		$safe_value = is_array($value) ? "!!array-" . count($value) : 
-						is_object($value) ? "!!object-" .get_class($value) :
-						is_resource($value) ? "!!resource-" . get_resource_type($value) :
-						is_null($value) ? "!!null" :
-						is_bool($value) ? "!!bool-" . ($value ? 'TRUE' : 'FALSE') :
-						 esc_html($value);
-		echo("<tr><td>$safe_name</td><td>$safe_value</td></tr>\n");
+	wtwp_echo_array_table($GLOBALS);
+}
+
+function wtwp_echo_subsystem_status() {
+	$name = 'wtwp-dummy-transient';
+	$value = 'wtwp-dummy-value';
+	$status = array();
+	$status['transient-set'] = set_transient($name, $value);
+	$status['transient-get'] = get_transient($name) == $value;
+	$status['transient-delete'] = delete_transient($name);
+	$status['option-set'] = update_option($name, $value);
+	$status['option-get'] = get_option($name) == $value;
+	$status['option-delete'] = delete_option($name);
+
+	foreach($status as &$v) {
+		$v = $v ? 'OK' : 'FAIL';
 	}
-	echo("</table>\n");
+
+	wtwp_echo_array_table($status);
 }
 	
 
 function wtwp_add_dashboard_widgets() {
+	wp_add_dashboard_widget('wtwp_table_of_status', 'WTWP Subsystem Status', 'wtwp_echo_subsystem_status');
 	wp_add_dashboard_widget('wtwp_table_of_constants', 'WTWP Table of Constants', 'wtwp_echo_constants_table');
 	wp_add_dashboard_widget('wtwp_table_of_globals', 'WTWP Table of Globals', 'wtwp_echo_globals_table');
 }
